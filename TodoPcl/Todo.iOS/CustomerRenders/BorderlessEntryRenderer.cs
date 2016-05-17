2144506b48d1521a82b83f956b0b75d78dad944f
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-05-18 01:15:03 +0700

    Remove unused files

---
+++ TodoPcl/Todo.iOS/CustomerRenders/BorderlessEntryRenderer.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using UIKit;
+using Todo.iOS;
+
+[assembly:ExportRenderer(typeof(Entry), typeof(BorderlessEntryRenderer))]
+namespace Todo.iOS
+{
+	public class BorderlessEntryRenderer : EntryRenderer
+	{
+		public BorderlessEntryRenderer ()
+		{
+		}
+
+		protected override void OnElementChanged (ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged (e);
+
+			if (this.Control == null) return;
+			//this.Control.BorderStyle = UITextBorderStyle.None;
+			Control.BackgroundColor = UIColor.LightTextColor;
+		}
+	}
+}
+
--- TodoPcl/Todo/App.cs
+++ TodoPcl/Todo/App.cs
@@ -11,42 +11,42 @@ namespace Todo
 		public App ()
 		{
 			Resources = new ResourceDictionary ();
-			Resources.Add ("primaryGreen", Color.FromHex("91CA47"));
+			Resources.Add ("primaryGreen", Color.FromHex ("91CA47"));
 			Resources.Add ("primaryDarkGreen", Color.FromHex ("6FA22E"));
 
 			var nav = new NavigationPage (new TodoItemListX ());
-			nav.BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"];
+			nav.BarBackgroundColor = (Color)App.Current.Resources ["primaryGreen"];
 			nav.BarTextColor = Color.White;
 
 			MainPage = nav;
 		}
 
 		public static TodoItemDatabase Database {
-			get { 
+			get {
 				if (database == null) {
 					database = new TodoItemDatabase ();
 				}
-				return database; 
+				return database;
 			}
 		}
 
 		public string ResumeAtTodoId { get; set; }
 
-		protected override void OnStart()
+		protected override void OnStart ()
 		{
 			Debug.WriteLine ("OnStart");
 
 			// always re-set when the app starts
 			// users expect this (usually)
-//			Properties ["ResumeAtTodoId"] = "";
+			Properties ["ResumeAtTodoId"] = "";
 			if (Properties.ContainsKey ("ResumeAtTodoId")) {
-				var rati = Properties ["ResumeAtTodoId"].ToString();
-				Debug.WriteLine ("   rati="+rati);
+				var rati = Properties ["ResumeAtTodoId"].ToString ();
+				Debug.WriteLine ("   rati=" + rati);
 				if (!String.IsNullOrEmpty (rati)) {
 					Debug.WriteLine ("   rati = " + rati);
 					ResumeAtTodoId = rati;
 
-					if (!string.IsNullOrEmpty(ResumeAtTodoId)) {
+					if (!string.IsNullOrEmpty (ResumeAtTodoId)) {
 						var todoPage = new TodoItemPageX ();
 						todoPage.BindingContext = Database.GetItem (ResumeAtTodoId);
 
@@ -57,34 +57,17 @@ namespace Todo
 				}
 			}
 		}
-		protected override void OnSleep()
+		protected override void OnSleep ()
 		{
 			Debug.WriteLine ("OnSleep saving ResumeAtTodoId = " + ResumeAtTodoId);
 			// the app should keep updating this value, to
 			// keep the "state" in case of a sleep/resume
 			Properties ["ResumeAtTodoId"] = ResumeAtTodoId;
 		}
-		protected override void OnResume()
+		protected override void OnResume ()
 		{
 			Debug.WriteLine ("OnResume");
-//			if (Properties.ContainsKey ("ResumeAtTodoId")) {
-//				var rati = Properties ["ResumeAtTodoId"].ToString();
-//				Debug.WriteLine ("   rati="+rati);
-//				if (!String.IsNullOrEmpty (rati)) {
-//					Debug.WriteLine ("   rati = " + rati);
-//					ResumeAtTodoId = int.Parse (rati);
-//
-//					if (ResumeAtTodoId >= 0) {
-//						var todoPage = new TodoItemPage ();
-//						todoPage.BindingContext = Database.GetItem (ResumeAtTodoId);
-//
-//						MainPage.Navigation.PushAsync (
-//							todoPage,
-//							false); // no animation
-//					}
-//				}
-//			}
+
 		}
 	}
 }
-
